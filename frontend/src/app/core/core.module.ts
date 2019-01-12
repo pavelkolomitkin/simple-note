f3eed11d74f47a9911d1148a4a3fe0dadaf6ad6a
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2019-01-12 16:05:55 +0400

    add bootstrap cs

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import {SharedModule} from '../shared/shared.module';
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    NotFoundPageComponent
+    NotFoundPageComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    NgbModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule { }
