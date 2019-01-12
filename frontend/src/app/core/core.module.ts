2d5457159bec057ecfac72469c5756d32d548420
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2019-01-12 20:07:00 +0400

    add application initializer

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,15 +1,17 @@
-import {isDevMode, NgModule} from '@angular/core';
+import {APP_INITIALIZER, isDevMode, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
-import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
-import {StoreDevtoolsModule} from "@ngrx/store-devtools";
-import {HttpClientModule} from "@angular/common/http";
-import SecurityService from "../security/services/security.service";
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {HttpClientModule} from '@angular/common/http';
+import SecurityService from '../security/services/security.service';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import {BaseApiUrlInterceptor} from "./services/interceptors/base-api-url.interceptor";
-import {DefaultHttpHeadersInterceptor} from "./services/interceptors/default-http-headers.interceptor";
-import {AuthTokenInjectorInterceptor} from "./services/interceptors/auth-token-injector.interceptor";
+import {BaseApiUrlInterceptor} from './services/interceptors/base-api-url.interceptor';
+import {DefaultHttpHeadersInterceptor} from './services/interceptors/default-http-headers.interceptor';
+import {AuthTokenInjectorInterceptor} from './services/interceptors/auth-token-injector.interceptor';
+import {LocalStorageService} from './services/local-storage.service';
+import {appInitializeHandler, AppInitializerService} from "./services/app-initializer.service";
 
 const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: BaseApiUrlInterceptor, multi: true },
@@ -35,7 +37,15 @@ const httpInterceptorProviders = [
   ],
   providers: [
     httpInterceptorProviders,
-    SecurityService
+    LocalStorageService,
+    SecurityService,
+    AppInitializerService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitializeHandler,
+      deps: [AppInitializerService],
+      multi: true
+    }
   ],
   exports: [
     HeaderComponent,
