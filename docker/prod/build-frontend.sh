6349fa7191864e0be58262e4b7368757e163ced1
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2019-01-27 01:06:47 +0400

    describe secrets

--- docker/prod/build-frontend.sh
+++ docker/prod/build-frontend.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -v $(pwd)/../../frontend:/app -w /app teracy/angular-cli:1.7.4 ng build --prod
+docker container run --rm -v $(pwd)/../../frontend:/app -w /app teracy/angular-cli:1.7.4 npm install && ng build --prod
