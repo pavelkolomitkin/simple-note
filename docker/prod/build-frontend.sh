44afceeff59a12d96ce747e81e46b9d1ec0bf50c
Pavel Kolomytkin <pavelkolomitkin@gmail.com>
2019-01-28 22:40:19 +0400

    remove docker prod directory

--- docker/prod/build-frontend.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker container run --rm -v $(pwd)/../../frontend:/app -w /app teracy/angular-cli:1.7.4 npm install
-docker container run --rm -v $(pwd)/../../frontend:/app -w /app teracy/angular-cli:1.7.4 ng build --prod
